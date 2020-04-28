2b8c962c86ba5e60103fe9f9142cfca436ed8e09
Romanova Elena <for-alisia@yandex.ru>
2020-04-28 20:33:59 +0500

    Simple server is ready

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    if (req.method === "GET") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (error, content) => {
+                    if (error) throw error;
 
-    res.write('Hello from NodeJS and Nodemon');
-    res.end();
+                    res.end(content);
+                }
+            )
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (error, content) => {
+                    if (error) throw error;
+
+                    res.end(content);
+                }
+            )
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json; charset=utf-8'
+            });
+
+            const users = [
+                {name: 'Jane', age: 30},
+                {name: 'Nick', age: 35}
+            ]
+
+            res.end(JSON.stringify(users));
+        }
+    } else if (req.method === "POST") {
+        const body = [];
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', (data) => {
+            body.push(Buffer.from(data));
+        });
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+
+            res.end(`
+                <h1>Your message: ${message}</h1>
+            `)
+        })
+    }
 
 });
 
